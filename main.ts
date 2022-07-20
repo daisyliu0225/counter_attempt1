ea48a10c1ed5dc59f455fcb900d465dad6d583db
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-20 21:24:08 +0800

    on start --> forever

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-let index = 0
-for (let index2 = 0; index2 < 11; index2++) {
-    basic.showNumber(index)
-    basic.pause(1000)
-    index += 1
-}
+basic.forever(function () {
+    for (let index = 0; index <= 10; index++) {
+        basic.showNumber(index)
+        basic.pause(1000)
+    }
+})
