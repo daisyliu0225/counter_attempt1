c00c77f1e2aa35b59000bc3e87fd417849fe224c
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-20 21:39:09 +0800

    counter with buttons A and B

--- main.ts
+++ main.ts
@@ -1,6 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    butA = 1
+})
+input.onButtonPressed(Button.B, function () {
+    butB = 1
+})
+let butB = 0
+let butA = 0
+butA = 0
+butB = 0
 basic.forever(function () {
-    for (let index = 0; index <= 10; index++) {
-        basic.showNumber(index)
-        basic.pause(1000)
+    if (butA == 1) {
+        for (let index = 0; index <= 10; index++) {
+            basic.showNumber(index)
+            basic.pause(200)
+            if (butB == 1) {
+                butB = 0
+                basic.clearScreen()
+                break;
+            }
+            butA = 0
+        }
     }
 })
